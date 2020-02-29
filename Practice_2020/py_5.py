b2c5f18ede701836fe769f154e47c3eabee168b1
gaga2455066166 <2455066166@qq.com>
2020-02-29 16:20:17 +0800

    2020-0229-py

---
+++ Practice_2020/py_5.py
@@ -0,0 +1,12 @@
+a = 90
+print(type(a))
+b = 2 ** 1000
+print(b)
+print(type(b))
+
+c = "陈"
+d = "ge"
+e = c + d
+print(e)
+print(c * 10)
+print(e * a)
---
+++ Practice_2020/py_6.py
@@ -0,0 +1,16 @@
+a = input("输入：")
+print("input -------str  "+a)
+print(type(a))
+
+b = int(a)
+print(b)
+print(type(b))
+
+
+c= float(a)
+print(c)
+print(type(c))
+
+d = float("121231.221212")
+print(d)
+print(type(d))
