081d054d4fc23a1b7136ff56413331a6a29183f0
gaga2455066166 <2455066166@qq.com>
2020-06-12 11:46:52 +0800

    20200420_pta_week6

--- py_pta/pta_fuxi_4.py
+++ py_pta/pta_fuxi_4.py
@@ -1,6 +1,29 @@
 # 输出全排列，p88，例题6-32
-# ['a', 'D', 'b', 'f', 'd']
-c = eval(input())
-print(sorted(c))
-d = [s.lower() for s in c if isinstance(s, str) == True]
-print(sorted(d))
+# # ['a', 'D', 'b', 'f', 'd']
+# c = eval(input())
+# print(sorted(c))
+# d = [s.lower() for s in c if isinstance(s, str) == True]
+# print(sorted(d))
+
+
+####################################
+# 输入样例：
+# 3
+# 输出样例：
+# 123
+# 132
+# 213
+# 231
+# 312
+# 321
+from itertools import permutations
+
+n = int(input())
+a = [str(i) for i in range(1, n + 1)]
+print(a)
+s = ""
+s = s.join(a)
+print(s)
+for i in permutations(s, n):
+    print(i)
+    print("".join(i))
---
+++ py_pta/pta_fuxi_5.py
@@ -0,0 +1,28 @@
+# 列表或元组的数字元素求和 (20分)
+# 求列表中数字和,列表中嵌套层次不限2层
+#
+# 输入格式:
+# 在一行中输入列表或元组
+#
+# 输出格式:
+# 在一行中输出数字的和
+#
+# 输入样例:
+# 在这里给出一组输入。例如：
+#
+# [11,2,[3,7],(68,-1),"123",9]
+# 输出样例:
+# 在这里给出相应的输出。例如：
+#
+# 99
+str_str = input()
+sum_str = 0
+str_str = str_str.replace('[', '')
+str_str = str_str.replace(']', '')
+str_str = str_str.replace('(', '')
+str_str = str_str.replace(')', '')
+int_str = eval(str_str)
+for i in int_str:
+    if isinstance(i, int):
+        sum_str += i
+print(sum_str)
