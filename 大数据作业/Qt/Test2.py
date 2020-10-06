35fd8ef97e7721a9ea1cd8a329f59e4d2baab097
gaga2455066166 <52930591+gaga2455066166@users.noreply.github.com>
2020-10-06 11:17:27 +0800

    qt

--- 大数据作业/Qt/Test2.py
+++ 大数据作业/Qt/Test2.py
@@ -1,50 +1,43 @@
 import sys
-import pcc
+import 1_rc
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_MainWindow(object):
-    def __init__(self):
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.formLayoutWidget = QtWidgets.QWidget(self.centralwidget)
-        self.pushButton = QtWidgets.QPushButton(self.formLayoutWidget)
-        self.formLayout = QtWidgets.QFormLayout(self.formLayoutWidget)
 
-    def setupUi(self, mainWindow):
-        mainWindow.setObjectName("MainWindow")
-        mainWindow.resize(800, 600)
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+        self.formLayoutWidget = QtWidgets.QWidget(self.centralwidget)
         self.formLayoutWidget.setGeometry(QtCore.QRect(280, 50, 451, 502))
         self.formLayoutWidget.setObjectName("formLayoutWidget")
+        self.formLayout = QtWidgets.QFormLayout(self.formLayoutWidget)
         self.formLayout.setContentsMargins(0, 0, 0, 0)
         self.formLayout.setObjectName("formLayout")
+        self.pushButton = QtWidgets.QPushButton(self.formLayoutWidget)
         self.pushButton.setMinimumSize(QtCore.QSize(50, 500))
         self.pushButton.setStyleSheet("border-image: url(:/newPrefix/1.jpg);")
         self.pushButton.setObjectName("pushButton")
         self.formLayout.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.pushButton)
-        mainWindow.setCentralWidget(self.centralwidget)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 23))
         self.menubar.setObjectName("menubar")
-        mainWindow.setMenuBar(self.menubar)
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
-        mainWindow.setStatusBar(self.statusbar)
-        # self.pushButton.clicked.connect(self.hello)
+        MainWindow.setStatusBar(self.statusbar)
 
-        self.retranslateUi(mainWindow)
-        QtCore.QMetaObject.connectSlotsByName(mainWindow)
-        self.pushButton.clicked.connect(self.hello)
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-    def retranslateUi(self, mainWindow):
+    def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        mainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.pushButton.setText(_translate("MainWindow", "大帅比"))
-
-    @staticmethod
-    def hello():
-        print("hello world")
 
 
 if __name__ == '__main__':
--- 大数据作业/Qt/testQt5.py
+++ 大数据作业/Qt/testQt5.py
@@ -1,46 +1,40 @@
 import sys
-import pcc
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5 import QtCore, QtGui, QtWidgets
 
+
+# XXXXXXXXXXX
+# XXXXX
+# XXXXX    #为生成的代码段
 
 class Ui_MainWindow(object):
     def __init__(self):
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.formLayoutWidget = QtWidgets.QWidget(self.centralwidget)
-        self.pushButton = QtWidgets.QPushButton(self.formLayoutWidget)
-        self.formLayout = QtWidgets.QFormLayout(self.formLayoutWidget)
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
 
     def setupUi(self, mainWindow):
         mainWindow.setObjectName("MainWindow")
         mainWindow.resize(800, 600)
         self.centralwidget.setObjectName("centralwidget")
-        self.formLayoutWidget.setGeometry(QtCore.QRect(280, 50, 451, 502))
-        self.formLayoutWidget.setObjectName("formLayoutWidget")
-        self.formLayout.setContentsMargins(0, 0, 0, 0)
-        self.formLayout.setObjectName("formLayout")
-        self.pushButton.setMinimumSize(QtCore.QSize(50, 500))
-        self.pushButton.setStyleSheet("border-image: url(:/newPrefix/1.jpg);")
+        self.pushButton.setGeometry(QtCore.QRect(170, 280, 200, 50))
         self.pushButton.setObjectName("pushButton")
-        self.formLayout.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.pushButton)
         mainWindow.setCentralWidget(self.centralwidget)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 23))
         self.menubar.setObjectName("menubar")
         mainWindow.setMenuBar(self.menubar)
         self.statusbar.setObjectName("statusbar")
         mainWindow.setStatusBar(self.statusbar)
-        # self.pushButton.clicked.connect(self.hello)
+        self.pushButton.clicked.connect(self.hello)
 
         self.retranslateUi(mainWindow)
         QtCore.QMetaObject.connectSlotsByName(mainWindow)
-        self.pushButton.clicked.connect(self.hello)
 
     def retranslateUi(self, mainWindow):
         _translate = QtCore.QCoreApplication.translate
         mainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.pushButton.setText(_translate("MainWindow", "大帅比"))
+        self.pushButton.setText(_translate("MainWindow", "打印hello world"))
 
     @staticmethod
     def hello():
